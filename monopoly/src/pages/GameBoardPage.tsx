55fbff5ac82ce0dce73f639081911d7f929ea801
HonzaVrbensky <vrbensky.honza@gmail.com>
2024-05-11 21:02:53 +0200

    end turn button

--- monopoly/src/pages/GameBoardPage.tsx
+++ monopoly/src/pages/GameBoardPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { GameContext } from './../../types/GameProvider';
 import Styles from './GameBoardPage.module.css';
@@ -6,13 +6,16 @@ import Styles2 from './HomePage.module.css';
 
 const GameBoardPage: React.FC = () => {
   const { state, dispatch } = useContext(GameContext);
+  const [diceRolled, setDiceRolled] = useState(false);
 
   const handleDiceRoll = () => {
     dispatch({ type: 'DICEROLL', player: state.players[state.currentPlayerIndex]});
+    setDiceRolled(true);
   };
 
   const handleEndTurn = () => {
     dispatch({ type: 'END_TURN', player: state.players[state.currentPlayerIndex]});
+    setDiceRolled(false);
   };
 
   const handleNewGame = () => {
@@ -84,8 +87,8 @@ if (state.gameOver) {
         <button style={{backgroundColor: currentPlayer.color, color: 'black', minWidth: "103px"}} onClick={handleDiceRoll}>Roll Dice</button>
         {isPurchasable && currentPlayer.money > currentSpace.price && currentSpace.owner == undefined && <button onClick={handleBuyProperty} style={{backgroundColor: currentPlayer.color, color: 'black', minWidth: "103px"}}>Buy Property</button>}
         {isPurchasable && isUpgradable && currentSpace.owner == currentPlayer.id && currentSpace.upgrades < 4 &&  (<button onClick={handleUpgradeProperty} style={{backgroundColor: currentPlayer.color, color: 'black', minWidth: "103px"}}>Upgrade Property</button>)}
-        <button onClick={handleEndTurn} style={{backgroundColor: currentPlayer.color, color: 'black'}}>End Turn</button>
-       { <button onClick={handleNewGame} style={{backgroundColor: "grey", color: 'black', marginTop: "5px"}}>New Game</button>}
+        {diceRolled == true  && <button onClick={handleEndTurn} style={{backgroundColor: currentPlayer.color, color: 'black'}}>End Turn</button>}
+        {<button onClick={handleNewGame} style={{backgroundColor: "grey", color: 'black', marginTop: "5px"}}>New Game</button>}
         <div className={Styles2["btn--small"]}>
           <Link style={{color: "black"}} to="/">Back to Home</Link>
         </div>
